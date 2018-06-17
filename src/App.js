beb7d059807207739978a8fa7967f571370360e4
Matthew Thorne <mwilsonthorne@gmail.com>
2018-06-17 14:56:39 +0100

    Created container and SolarSystem.js and tested ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import SolarSystem from './container/SolarSystem.js';
 
 class App extends Component {
   render() {
     return (
-      "Hello World!"
+      <SolarSystem />
     );
   }
 }
---
+++ src/container/SolarSystem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div className="solar-system">
+        Hello World! I am a solar system
+      </div>
+
+    );
+  }
+}
+
+export default SolarSystem;
