106b9f998cf8e08a857404bc788a2df56b64ccca
Matthew Thorne <mwilsonthorne@gmail.com>
2018-06-17 15:39:06 +0100

    Added main header on SolarSystem.js

--- src/container/SolarSystem.js
+++ src/container/SolarSystem.js
@@ -4,7 +4,10 @@ import PlanetBox from '../component/PlanetBox.js';
 class SolarSystem extends Component {
   render() {
     return (
+      <div>
+        <h1>Our Solar System</h1>
         <PlanetBox />
+        </div>
     );
   }
 }
