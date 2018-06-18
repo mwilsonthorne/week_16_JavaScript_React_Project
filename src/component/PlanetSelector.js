87217adbaeda279f2caf01a6a42320a240a52d2e
Matthew Thorne <mwilsonthorne@gmail.com>
2018-06-18 10:53:43 +0100

    Added Header in planetSelector.js

--- src/component/PlanetSelector.js
+++ src/component/PlanetSelector.js
@@ -5,6 +5,7 @@ class PlanetSelector extends Component {
   render(){
     return (
       <div className="planet-selector">
+      <h1>My Solar System</h1>
         Hello World! I am 2nd bottom of the hierarchy
       <PlanetData />
       </div>
