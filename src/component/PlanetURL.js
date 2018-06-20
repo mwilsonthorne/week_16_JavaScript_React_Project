49c37cdeea4d48165f3614d7761892b6067e8b25
Shoogaboog <lewisjohnmclean@gmail.com>
2018-06-20 17:17:58 +0100

    Added a new video player component that allows user to stream videos from youtube.

--- src/component/PlanetURL.js
+++ src/component/PlanetURL.js
@@ -1,3 +1,3 @@
-const PlanetURL = ["https://www.youtube.com/watch?v=IQL53eQ0cNA"]
+const PlanetURL = ["https://www.youtube.com/watch?v=m3ZUhpisWeQ", "https://www.youtube.com/watch?v=yzqbN6z8ncc", "https://www.youtube.com/watch?v=NXbCNAIIAxw", "https://www.youtube.com/watch?v=jQRbxjskrPc", "https://www.youtube.com/watch?v=aFnNCwTkYo8", "https://www.youtube.com/watch?v=AyFMPdHU1n0", "https://www.youtube.com/watch?v=_aRAoM1i0D4", "https://www.youtube.com/watch?v=faKnto4Pb6c", "https://www.youtube.com/watch?v=6l4kr36TzQ4"]
 
 export default PlanetURL;
