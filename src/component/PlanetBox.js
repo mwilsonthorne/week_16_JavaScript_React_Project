0f85a21226c6b4834d96034e751090cd82f2da95
Matthew Thorne <mwilsonthorne@gmail.com>
2018-06-20 11:18:56 +0100

    Added Planet Data

--- src/component/PlanetBox.js
+++ src/component/PlanetBox.js
@@ -1,32 +1,31 @@
-  import React from 'react';
-  // import PlanetSelector from './PlanetSelector.js';
+import React from 'react';
+// import PlanetSelector from './PlanetSelector.js';
 
-  const PlanetBox = (props) => {
-
-
-    const planetInfo = props.planets.map((planet, index) =>{
-      // const planetIndex = index
-      const imgID =  `/images/${planet.id}.jpg`
+const PlanetBox = (props) => {
 
 
-      function handleChange(event){
-        props.onPlanetSelected(event.currentTarget.getAttribute('value'));
-      }
+  const planetInfo = props.planets.map((planet, index) =>{
+    // const planetIndex = index
+    const imgID =  `/images/${planet.id}.jpg`
 
-      return (
-        <div key={index} value={planet.id} onClick={handleChange}>
-          <h2> {planet.name} </h2>
-          <img src={window.location.origin + imgID} alt={planet.name} className="planetImage"/>
-        </div>
-)
 
-})
-      return (
-        <div>
-          {planetInfo}
-        </div>
-        // <PlanetSelector />
+    function handleChange(event){
+      props.onPlanetSelected(event.currentTarget.getAttribute('value'));
+    }
 
-      );
-  }
+    return (
+      <div key={index} value={planet.id} onClick={handleChange}>
+        <h2> {planet.name} </h2>
+        <img src={window.location.origin + imgID} alt={planet.name} className="planetImage"/>
+      </div>
+    )
+
+  })
+  
+  return (
+    <div>
+      {planetInfo}
+    </div>
+  );
+}
   export default PlanetBox;
--- src/component/PlanetData.js
+++ src/component/PlanetData.js
@@ -4,14 +4,19 @@ import React from 'react';
 const PlanetData = (props) => {
   if(!props.planet) return null
 
-
-
-
-
     return (
       <div className="planet-data">
-        <h3>{props.planet.name}</h3>
-        <h3>{props.planet.gravity}</h3>
+        <h3>Planet Name: {props.planet.name}</h3>
+        <h3>Mass: {props.planet.mass} e24kg</h3>
+        <h3>Density: {props.planet.density} kg/m^3</h3>
+        <h3>Diameter: {props.planet.diameter} km</h3>
+        <h3>Orbital Velocity: {props.planet.orbital_velocity} km/sec</h3>
+        <h3>Gravity: {props.planet.gravity} metres/sec^2</h3>
+        <h3>Day Length: {props.planet.length_of_day} hrs</h3>
+        <h3>Year Length: {props.planet.orbital_period} days</h3>
+        <h3>Distance from the Sun: {props.planet.distance_from_sun} million km</h3>
+        <h3>Temperature: {props.planet.mean_temperature} degrees Celsius</h3>
+        <h3>Moons: {props.planet.number_of_moons}</h3>
       </div>
     );
 
